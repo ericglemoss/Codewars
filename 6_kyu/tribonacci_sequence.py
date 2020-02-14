c364722a5f89eaadbe4e6127315c85a58842bc51
Eric Lemos <ericglemos@usp.br>
2020-02-14 19:41:11 -0300

    Update tribonacci_sequence.py

--- 6_kyu/tribonacci_sequence.py
+++ 6_kyu/tribonacci_sequence.py
@@ -2,7 +2,7 @@
 # numbers of the sequence to generate the next.
 
 # Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns 
-the first n elements -  signature included of the so seeded sequence.
+# the first n elements -  signature included of the so seeded sequence.
 
 # Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array.
 
