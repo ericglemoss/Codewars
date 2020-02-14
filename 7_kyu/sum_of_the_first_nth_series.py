b3f8b464c940b37e78da3591f35074d469bbd9c7
Eric Lemos <48073499+ericglemoss@users.noreply.github.com>
2020-02-14 18:27:12 -0300

    Create sum_of_the_first_nth_series.py

---
+++ 7_kyu/sum_of_the_first_nth_series.py
@@ -0,0 +1,10 @@
+# Your task is to write a function which returns the sum of following series upto nth term(parameter).
+# Series: 1 + 1/4 + 1/7 + 1/10 +...
+
+#Rules:
+# -You need to round the answer to 2 decimal places and return it as String.
+# -If the given value is 0 then it should return 0.00
+# -You will only be given Natural Numbers as arguments.
+
+def series_sum(n):
+   return "{:.2f}".format(sum([1/(3*i + 1) for i in range(n)]) if n!=0 else 0) 
