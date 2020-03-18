95fbd73340b3c687d4b5727d0b2006cf3a860938
Eric Lemos <ericglemos@usp.br>
2020-03-18 11:13:24 -0300

    Create all_code_star_challenge_4.py

---
+++ beta_kyu/all_code_star_challenge_4.py
@@ -0,0 +1,4 @@
+# Create a function, called getCollege, that takes in an object of an nba player, and returns the college that the player went to.
+
+def getCollege(player):
+    return player['college']
