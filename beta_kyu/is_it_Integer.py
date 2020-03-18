cc41546cbac1763c6fafd430acb30d9c494b2b5e
Eric Lemos <ericglemos@usp.br>
2020-03-18 11:06:58 -0300

    Create is_it_Integer.py

---
+++ beta_kyu/is_it_Integer.py
@@ -0,0 +1,7 @@
+# In this Kata we are passing a number (n) into a function.
+# Your code will determine if the number passed is integer (or not).
+# The function needs to return either a true or false.
+# Numbers may be positive or negative, string or null.
+
+def isInt(n):
+    return isinstance(n,int)
