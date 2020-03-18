8eca7edbf60c48594a5655ca275d700468300e0c
Eric Lemos <ericglemos@usp.br>
2020-03-18 11:07:44 -0300

    Create learn_python_1.py

---
+++ beta_kyu/learn_python_1.py
@@ -0,0 +1,5 @@
+# Your mission, should you choose to accept it, is to create a variable called name with the value being your name, 
+# in quotation marks, and tha variable age with your age as a number, with no quotation marks.
+
+name = 'Eric'
+age = 21
