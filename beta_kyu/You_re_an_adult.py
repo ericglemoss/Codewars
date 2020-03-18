da9e0e80342d8f51a645870ffaa7288dc0cf3a55
Eric Lemos <ericglemos@usp.br>
2020-03-18 11:14:18 -0300

    Create You_re_an_adult.py

---
+++ beta_kyu/You_re_an_adult.py
@@ -0,0 +1,5 @@
+# his kata is very simple, input your age, then output You're an adult or You're a minor.
+# An adult means the person's age over 18. Input will be always >= 0.
+
+def is_adult(age):
+    return "You're an adult" if age>=18 else "You're a minor"
